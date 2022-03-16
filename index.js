71cecefc270f7af5b2c3cef1f0ba4ff5f7b5522f
louie_gomez <kinggomez027.lg@gmail.com>
2022-03-16 13:16:12 -0400

    commit tree 3835889fa202eda7661048eea0aa3bc77c6b64d6

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+    const num1 = 31
+    const num2 = 2
+    let multiply = num1 * num2;
 
-// Write your code here
+    let random = Math.random();
+    Math.floor(Math.random() * 10) + 1;
+    random = Math.ceil(random);
+    console.log(Number.isInteger(random));
+
+    const num3 = 8
+    const num4 = 2
+    let mod = num3 / num4;
+
+    let max = Math.max(5, 10, 15, 20);
+    console.log(max);
